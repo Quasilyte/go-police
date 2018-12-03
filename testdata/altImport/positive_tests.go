7a28db8d53399350a8d3e10f3ac29543f18c961d
Iskander Sharipov <quasilyte@gmail.com>
2018-12-03 18:21:22 +0300

    use new lintpack warning directive format
    This fixes tests that are broken right now.
    
    Signed-off-by: Iskander Sharipov <quasilyte@gmail.com>

--- testdata/altImport/positive_tests.go
+++ testdata/altImport/positive_tests.go
@@ -1,10 +1,10 @@
 package checker_test
 
 import (
-	/// don't import "errors", use fmt.Errorf instead
+	/*! don't import "errors", use fmt.Errorf instead */
 	"errors"
 
-	/// don't import "path", use path/filepath package instead
+	/*! don't import "path", use path/filepath package instead */
 	"path"
 )
 
